20f2e044c95f5464bbb1b5ec7b57bd8db34d93f9
Murad011 <muradaydin122@gmail.com>
2021-01-14 01:43:49 +0400

    Form Validations Created

---
+++ src/data.js
@@ -0,0 +1,15 @@
+module.exports = function() {
+    return {
+        countries: [
+            {"iso" : "AZ", "country" : "Azerbaijan"},
+            {"iso" : "AF", "country" : "Afghanistan"},
+            {"iso" : "AL", "country" : "Albania"},
+            {"iso" : "BS", "country" : "Bahams"},
+            {"iso" : "BH", "country" : "Bahrain"},
+            {"iso" : "YE", "country" : "Yemen"},
+            {"iso" : "TR", "country" : "Turkey"},
+            {"iso" : "ZM", "country" : "Zambia"},
+            {"iso" : "ZW", "country" : "Zimbabwe"},
+        ]
+    }
+}
